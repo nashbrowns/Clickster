b5867d94373fcbbd2836979642095586ef82d623
Nash McDonald <nash42192@gmail.com>
2019-06-04 18:05:54 -0700

    turned cardgame into class with state

--- client/src/pages/CardGame.js
+++ client/src/pages/CardGame.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
-//import CardContainer from '../components/CardContainer';
 import Card from '../components/Card';
 
-function CardGame() {
-    return(
-        <div>
-            <Nav />
-            <container className="Card-Container">
-                <div class="row">
-                    <div class="col">
-                        <Card />
+class CardGame extends Component {
+
+    state = {
+        //
+      };
+
+    render() {
+        return (
+            <div>
+                <Nav />
+                <container className="Card-Container">
+                    <div class="row">
+                        <div class="col">
+                            <Card />
+                        </div>
+                        <div class="col">
+                            <Card />
+                        </div>
                     </div>
-                    <div class="col">
-                        <Card />
-                    </div>
-                </div>
-            </container>
-            <Footer />
-        </div>
-    );
+                </container>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default CardGame;
